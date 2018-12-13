3a85f8febb774ae1c8e8ca786402a4a5ccd14916
harapan <harapanpardamean@gmail.com>
2018-12-14 00:44:12 +0700

    .

--- src/Routes/Mapel/Semester.js
+++ src/Routes/Mapel/Semester.js
@@ -3,17 +3,31 @@ import React, { Component } from "react";
 export default class Semester extends Component {
   render() {
     const { className, semester, CallBack } = this.props;
-    return (
-      <div className={className ? className : "uk-padding-small"}>
-        <select
-          className="uk-select"
-          value={semester}
-          onChange={e => CallBack(e.target.value)}
-        >
-          <option value={1}>Semester 1</option>
-          <option value={2}>Semester 2</option>
-        </select>
-      </div>
-    );
+    switch (this.props.gaya) {
+      case 1:
+        return (
+          <div uk-form-custom="target: true">
+            <select value={semester} onChange={e => CallBack(e.target.value)}>
+              <option value={1}>Semester 1</option>
+              <option value={2}>Semester 2</option>
+            </select>
+            <span />
+          </div>
+        );
+
+      default:
+        return (
+          <div className={className ? className : "uk-padding-small"}>
+            <select
+              className="uk-select"
+              value={semester}
+              onChange={e => CallBack(e.target.value)}
+            >
+              <option value={1}>Semester 1</option>
+              <option value={2}>Semester 2</option>
+            </select>
+          </div>
+        );
+    }
   }
 }
--- src/Routes/Raport/ContentRaport.js
+++ src/Routes/Raport/ContentRaport.js
@@ -1,35 +1,45 @@
 import React, { Component } from "react";
 import Semester from "../Mapel/Semester";
-
+import { DbGuru } from "../../data";
 export default class ContentRaport extends Component {
   render() {
     return (
-      <table className="uk-table uk-table-justify uk-table-divider">
-        <thead>
-          <tr>
-            <th className="uk-width-small">Nama Murid </th>
-            <th className="uk-padding-remove uk-width-small">
-              <Semester
-                className="uk-padding-remove uk-margin-remove"
-                semester={this.props.semester}
-                CallBack={this.props.CallBack}
-              />
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Nama Murid</td>
-            <td>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </td>
-            <td>
-              <button className="uk-button uk-button-text">Text</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <table className="uk-table uk-table-middle uk-table-divider uk-width-1-1">
+          <thead>
+            <tr>
+              <th className="uk-width-1-4">Murid</th>
+              <th className="uk-width-2-4">
+                <Semester
+                  className="uk-padding-remove uk-margin-remove"
+                  semester={this.props.semester}
+                  CallBack={this.props.CallBack}
+                  gaya={1}
+                />
+              </th>
+              <th className="uk-width-1-4" />
+            </tr>
+          </thead>
+          <tbody>
+            {DbGuru.murid().map(murid => (
+              <tr key={murid.id}>
+                {console.log(murid)}
+                <td>{murid.nama}</td>
+                <td>
+                  <div className="uk-card uk-card-default uk-card-body uk-width-auto">
+                    <p>L</p>
+                  </div>
+                </td>
+                <td>
+                  <button className="uk-button uk-button-default" type="button">
+                    Simpan
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
